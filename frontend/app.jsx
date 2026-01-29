d406b58cd33ac20a26e554a7aaa4e25f9b2fa9e1
Vinicius Gabriel de Lima Fiel <viniciuslima09321@gmail.com>
2026-01-29 00:22:48 -0300

    Update app.jsx

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { LayoutDashboard, Package, Beaker, BarChart3, Users, Settings, LogOut, ChevronDown, Table, FileText, ShieldCheck } from 'lucide-react';
 
-const API_URL = "https://seu-backend-aqui.vercel.app"; // Coloque aqui o link do backend
+const API_URL = "SUA_URL_DA_VERCEL_BACKEND"; // Coloque aqui o link do backend
 
 const App = () => {
   const [logado, setLogado] = useState(false);
